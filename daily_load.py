0602d52473320f2973cabaecc798a4b201360059
seekaddo <dennis_addo@aol.com>
2023-09-02 20:39:26 +0200

    updated logic to add new data to the list

--- daily_load.py
+++ daily_load.py
@@ -17,21 +17,18 @@ eml_url = "https://www.win2day.at/lotterie/euromillionen"
 # euarch_url = "https://www.euro-millions.com/de-at/{}-zahlen-archiv"
 yr_count = 2023
 
-#eml_url = euarch_url.format(yr_count)
+# eml_url = euarch_url.format(yr_count)
 file_name = "{}_eml.json".format(yr_count)
-
 
 import requests
 from bs4 import BeautifulSoup
 
 
 def extract_balls(dac):
-
     # Find all the <li> elements with the class "resultBall" or "lucky-star"
     balls_raw = dac.find_all("span", class_=["ball", "star"])
     lucky_stars = []
     balls = []
-
 
     # Loop through each <li> element and get its text content and class attribute value
     for ball in balls_raw:
@@ -56,13 +53,13 @@ def save_to_jsonl(out_file_name, data_out):
     with open(out_file_name, "w") as f:
         json.dump(data_out, f)
 
+
 def rdata_json(filepath):
     if os.path.exists(filepath):
         with open(filepath, "r") as f:
-            return json.load(f) # Pass the file object here
+            return json.load(f)  # Pass the file object here
     else:
         return {}
-
 
 
 # Create a session object that can store cookies
@@ -81,30 +78,29 @@ items_acord = soup.find("div", {"class": "accordion"})
 
 items_acord = items_acord.find_all("div", {"class": "accordion-item"})
 
-
 dict_eml = {}
 
 # Print the content of each result_row element
 for result_row in items_acord:
     span = result_row.find("span", {"class": "accordion-title"})
-    drawdiv = result_row.find("div", {"class": "win-numbers" })
-    #dr_date = result_row.get("title").split(",")[1]
-    #drw_lst = extract_balls(data)
+    drawdiv = result_row.find("div", {"class": "win-numbers"})
+    # dr_date = result_row.get("title").split(",")[1]
+    # drw_lst = extract_balls(data)
     text = span.get_text()
     datedr = text.split(",")[1].strip()
     draml = extract_balls(drawdiv)
     dict_eml[datedr] = draml
 
-    #print("Date:{0} --> {1}".format(datedr, draml ))
+    # print("Date:{0} --> {1}".format(datedr, draml ))
 
 print(dict_eml)
 file_name = "./dataset/{}_eml.json".format(yr_count)
 logging.info("Loading json eml data... at: {}".format(file_name))
 dryr_data = rdata_json(file_name)
-#print(dryr_data)
+# print(dryr_data)
 
-for k,v in dict_eml.items():
+for k, v in dict_eml.items():
     if k not in dryr_data:
-        logging.info("Data not found: --> add {0}:{1}".format(k,v))
+        logging.info("Data not found: --> add {0}:{1}".format(k, v))
         dryr_data[k] = v
 save_to_jsonl(file_name, dryr_data)
